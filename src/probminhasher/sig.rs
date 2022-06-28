f351b4310ca3d6fcb0c95a41013cb6c5c4f9f0d7
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-06-28 16:01:59 +0200

    signature chaged to Vec<u8>

--- src/probminhasher/sig.rs
+++ src/probminhasher/sig.rs
@@ -5,10 +5,10 @@
 //! the object uniquely
 
 
-/// return a u8 slice identifying the object.
+/// return a Vec<u8> identifying the object.
 /// For example a usize could return to_le_bytes, a 2-uple v = (usize, usize) could return
 /// the concatenation of slices obtained for v.0 and v.1.
-/// A String can return String.into::<Vec<u8>>
+/// A String can use get_sig from trait Sig.  
 /// The purpose of this is to get a u8 slice that can be fed into Sha update methods
 pub trait Sig {
     /// returns the object signature
@@ -17,8 +17,16 @@ pub trait Sig {
 
 
 impl Sig for Vec<u8>  {
-
     fn get_sig(&self) -> Vec<u8> {
         return self.clone();
     }
 } // end of impl Sig for <Vec<u8>>
+
+
+
+impl Sig for String {
+    fn get_sig(&self) -> Vec<u8> {
+        let s : &[u8] = self.as_ref();
+        return s.to_vec();
+    }   
+}
