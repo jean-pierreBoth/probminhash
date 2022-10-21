b13fbdea2b15cad974341f777d8674b9ac2a6eac
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-10-21 17:54:44 +0200

    typos...

--- src/probminhasher/sig.rs
+++ src/probminhasher/sig.rs
@@ -1,11 +1,11 @@
-//! a Trait for object needing to provide an id in the form of a [u8] slice possibly
-//! larger than 8 (corresponding to u64) to feed in Sha hash functions with  
+//!
+//! a Trait for object needing to provide an id in the form of a \[u8\] slice to be fed in Sha hash functions with  
 //!
 //! The purpose is to get a hash as perfect as possible so the slice returned must identify
 //! the object uniquely
 
 
-/// return a Vec<u8> identifying the object.
+/// return a Vec\<u8\> identifying the object.  
 /// For example a usize could return to_le_bytes, a 2-uple v = (usize, usize) could return
 /// the concatenation of slices obtained for v.0 and v.1.
 /// A String can use get_sig from trait Sig.  
