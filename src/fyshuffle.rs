5c0233f75927dd473e692ecf541962c0f942015d
Jean-Pierre Both <jeanpierre.both@gmail.com>
2023-02-13 19:35:57 +0100

    fyshuffle test added. Do not reset anymore after m so test2 pass!

--- src/fyshuffle.rs
+++ src/fyshuffle.rs
@@ -30,11 +30,9 @@ impl FYshuffle {
     /// generates next randomly choosen item of the set (you get sampling without replacement)
     /// After being call m times, it is possible to get the full permutation with the function get_values
     /// as the permutation is fully sampled.
-    /// If called more than m times, it calls reset implicitly to generate a new permutation.
-    /// It is possible (and recommended) to reset explicitly after m successive call to next method
     pub fn next(&mut self, rng : &mut impl rand::Rng) -> usize {
         if self.lastidx >= self.m {
-            self.reset();
+            self.lastidx = 0;
             log::debug!("FYshuffle next calling reset ");
         }
         let xsi = self.unif_01.sample(rng);
@@ -78,13 +76,15 @@ fn log_init_test() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
+use std::collections::HashMap;
+
 use super::*;
 
 
 #[test]
 // We check we have a unifom distribution of values at each rank of v
 // variance is 5/4
-fn test_fyshuffle() {
+fn test_fyshuffle_1() {
 
     log_init_test();
     let mut rng = Xoshiro256PlusPlus::seed_from_u64(45679 as u64);
@@ -113,14 +113,55 @@ fn test_fyshuffle() {
         assert!( rel_error.abs() < 3.)
     }
     info!("  freq = {:?}", freq);
-    for _ in 0..15 {
-        for _ in 0..m {
-            fypermut.next(&mut rng);
-        }
-        println!("permut state {:?} ", fypermut.get_values());
-        fypermut.reset();
-    }
-} // end of test_fyshuffle
 
+} // end of test_fyshuffle_1
+
+
+#[test]
+fn test_fyshuffle_2() {
+    //
+    log_init_test();
+    //
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64(45679 as u64);
+    let m = 4;
+    let mut p_count = HashMap::<[usize;4], usize>::new();
+
+    let mut h = [0usize; 4];
+    let mut fypermut = FYshuffle::new(m);
+    let nb_iterations = 1000000;
+    for _ in 0..nb_iterations {
+        for _ in 0..m+5 {
+            let _val = fypermut.next(&mut rng);
+            // hash values
+ 
+        } // end loop on m
+        let values = fypermut.get_values();
+        for i in 0..m {
+            h[i] = values[i];
+        }
+        match p_count.get_mut(&h) {
+            Some(count) => {
+                *count = *count + 1; 
+            }
+            _                       => {
+                p_count.insert(h, 1);
+            }
+        }
+        fypermut.reset();
+    } // end on iterations
+    // check all 24 permut are in equal count
+    assert_eq!(p_count.len(), 24);
+    let count = p_count.into_values().collect::<Vec<usize>>();
+    log::info!("count permutations : {:?}", count);
+    let mu_th = nb_iterations as f64 / 24 as f64;
+    let sigma = (1./(24.) * (23. / 24.) * nb_iterations as f64).sqrt();
+    log::info!("sigma for count of each permutation: {:.3e}", sigma);
+
+    let nb_s = (0..count.len()).into_iter().map(|i| (count[i] as f64 - mu_th)/ sigma).collect::<Vec<f64>>();
+    for i in 0..count.len() {
+        log::info!("nb sigma : {:.3e}", nb_s[i]);
+        assert!(nb_s[i].abs() < 3.);
+    }
+} 
 
 }
